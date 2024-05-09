f33db07664d74553c85379f9cbf7087cce69c043
Newsha Hosseiny Nik <newshahn@gmail.com>
2024-05-09 15:50:46 +0200

    test: add 1 unit test for delete hero in heroes component

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,36 @@
+import { of } from 'rxjs';
+import { HeroesComponent } from './heroes.component';
+
+describe('HeroesComponent', () => {
+  let component: HeroesComponent;
+  let HEROES;
+  let mockHeroService;
+
+  beforeEach(() => {
+    HEROES = [
+      { id: 1, name: 'SpiderDude', strength: 8 },
+      { id: 2, name: 'Brave', strength: 100 },
+      { id: 3, name: 'Gwen', strength: 200 },
+    ];
+
+    mockHeroService = jasmine.createSpyObj([
+      'getHeroes',
+      'addHero',
+      'deleteHero',
+    ]);
+
+    component = new HeroesComponent(mockHeroService);
+  });
+
+  describe('delete', () => {
+    it('should remove the indicated hero from the heroes list', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true));
+
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(component.heroes.length).toBe(2);
+    });
+  });
+});
